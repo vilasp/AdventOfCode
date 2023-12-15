9a93d826f8d5b978e3d7e0cf4b44445017f77fa1
villiam aspegren <villiam.aspegren@gmail.com>
2023-12-15 17:04:29 +0100

    feat: assignment 1 solution

--- 2023/assignment-01/assignment-01.ts
+++ 2023/assignment-01/assignment-01.ts
@@ -2,16 +2,24 @@ import {open} from 'node:fs/promises'
 
 // number iterator
 function numberAssembler(input: string) {
-  const numbers = input.match(/^\d+|\d+\b|\d+(?=\w)/g)
-  const numberArr = numbers?.map(numberString => +numberString) || [0, 0]
-  return numberArr[0] + numberArr[numberArr.length - 1]
+  const numbers = input.match(/^\d|\d\b|\d(?=\w)/g)
+  const numberArr = numbers?.map(numberString => numberString) || []
+  if (numberArr.length === 1) {
+    return parseInt(numberArr[0] + numberArr[0], 10)
+  }
+  if (numberArr.length > 1) {
+    return parseInt(numberArr[0] + numberArr[numberArr.length - 1], 10)
+  }
+
+  return 0
 }
 
 async function assignment1() {
   let theKey = 0
   const file = await open(process.cwd() + '/2023/assignment-01/input.txt')
   for await (const line of file.readLines()) {
-    theKey += numberAssembler(line)
+    const numberAssembled = numberAssembler(line)
+    theKey += numberAssembled
   }
 
   return theKey
