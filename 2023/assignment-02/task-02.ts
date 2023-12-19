108bc0c58eea11d4298f0b8ea15470c857400ecd
villiam aspegren <villiam.aspegren@gmail.com>
2023-12-19 17:26:24 +0100

    feat: add solution for assignment 02 task 2

---
+++ 2023/assignment-02/task-02.ts
@@ -0,0 +1,77 @@
+import {open} from 'node:fs/promises'
+
+type Game = {
+  red: number
+  blue: number
+  green: number
+  id: number
+}
+
+const CONFIGURATION = {
+  red: 12,
+  green: 13,
+  blue: 14,
+}
+
+function parseCubesFromGame(game: string): Game {
+  const maxCubesByColor = {
+    red: 0,
+    blue: 0,
+    green: 0,
+    id: 0,
+  }
+
+  for (const match of game.matchAll(
+    /(?<id>(?<=Game )\d+)|(?<blue>\d+(?= blue))|(?<red>\d+(?= red))|(?<green>\d+(?= green))/g,
+  )) {
+    const numberOfCubes = parseInt(match[0], 10)
+
+    if (match.groups?.red && numberOfCubes > maxCubesByColor.red) {
+      maxCubesByColor.red = numberOfCubes
+    } else if (match.groups?.blue && numberOfCubes > maxCubesByColor.blue) {
+      maxCubesByColor.blue = numberOfCubes
+    } else if (match.groups?.green && numberOfCubes > maxCubesByColor.green) {
+      maxCubesByColor.green = numberOfCubes
+    } else if (match.groups?.id) {
+      maxCubesByColor.id = numberOfCubes
+    }
+  }
+
+  return maxCubesByColor
+}
+
+function validateGameAgainstGameConfiguration(game: Game): boolean {
+  if (
+    game['red'] <= CONFIGURATION['red'] &&
+    game['blue'] <= CONFIGURATION['blue'] &&
+    game['green'] <= CONFIGURATION['green']
+  ) {
+    return true
+  }
+  return false
+}
+
+function gamePower(game: Game) {
+  let gamePower = 1
+  const {id, ...gameCubes} = game
+
+  Object.entries(gameCubes).forEach(([key, value]) => {
+    gamePower *= value
+  })
+
+  console.log(gamePower)
+  return gamePower
+}
+
+async function task() {
+  let theKey = 0
+  const file = await open(process.cwd() + '/2023/assignment-02/input.txt')
+  for await (const line of file.readLines()) {
+    const game = parseCubesFromGame(line)
+    theKey += gamePower(game)
+  }
+
+  return theKey
+}
+
+console.log(await task())
